43b010c2075c30ade09aeac738a132f541bb2ec3
hungluong <luongquochung.249@gmail.com>
2020-09-13 22:32:32 +0700

    first resource

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+
+@section('meta')
+	
+@endsection
+
+@section('title')
+	Homepage
+@endsection
+
+@section('view')
+	<div class="homepage">
+		
+	</div>
+@endsection
+
+@section('css')
+	<link rel="stylesheet" href="{{ asset('dist/scss/css/homepage.css') }}">
+@endsection
+
+@section('js')
+	
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="author" content="Pixel Media">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		
+		@yield('meta')
+
+		<!-- ZAYEKI CSS -->
+		<link rel="stylesheet" href="{{ asset('plugins/zayeki/css/zayeki.min.css')}}">
+		@yield('css')
+
+		<link rel="shortcut icon" href="{{ asset('dist/img/favicon.ico') }}" type="image/x-icon">
+    	<link rel="icon" href="{{ asset("dist/img/favicon.ico") }}" type="image/x-icon">
+
+		<title>@yield('title')</title>
+	</head>
+	<body>
+		@include('layouts._header')
+		
+		@yield('view')
+
+		@include('layouts._footer')
+		
+		<!-- JQUERY JS -->
+		<script src="{{ asset('plugins/jquery/jquery-3.4.1.min.js')}}"></script>
+
+		<!-- POPPER JS -->
+		<script src="https://unpkg.com/@popperjs/core@2"></script>
+
+		<!-- ZAYEKI JS-->
+		<script src="{{ asset('plugins/zayeki/js/zayeki.js')}}"></script>
+		<!-- ZAYEKI UTILS JS-->
+		<script src="{{ asset('plugins/zayeki/js/utils.js')}}"></script>
+
+		@yield('js')
+	</body>
+</html>
