82a2dff2d1240612288e10392d10d47f6ef8f8ef
hungluong <luongquochung.249@gmail.com>
2020-09-28 00:20:06 +0700

    edit homepage

--- public/dist/js/homepage/function.js
+++ public/dist/js/homepage/function.js
@@ -25,27 +25,27 @@ $(document).ready(function () {
 			.addClass('active');
 	});
 
+	scroll.on('call', (obj) => {});
+
 	// INIT YOUTUBE BACKGROUND
 	$('.section.section-product').each(function () {
 		if ($(this).find('.play-video-wrapper').length > 0) {
 			let ytbId = $(this).find('.play-video-wrapper').data('ytb-id');
-
 			$(this)
 				.find('.play-video')
 				.YTPlayer({
 					fitToBackground: true,
 					videoId: ytbId,
-					mute: true,
+					mute: false,
 					playerVars: {
 						modestbranding: 0,
-						autoplay: 1,
+						autoplay: 0,
 						controls: 0,
 						showinfo: 0,
 						branding: 0,
 						rel: 0,
 						autohide: 0,
 						start: 0,
-						end: 60,
 						origin: window.location.origin
 					}
 				});
@@ -53,8 +53,34 @@ $(document).ready(function () {
 	});
 });
 
+function playVideo(element) {
+	let videoPlay = $(element).closest('.section').find('.play-video').data('ytPlayer').player;
+
+	if (!$(element).data('play')) {
+		videoPlay.playVideo();
+
+		$(element).closest('.section').find('.play-video-overlay').fadeOut();
+		$(element).closest('.section').find('.section-overlay').addClass('hide');
+
+		$(element).html('<i class="elo el-lg el-pause"></i>');
+		$(element).data('play', true);
+	} else {
+		videoPlay.pauseVideo();
+
+		$(element).closest('.section').find('.play-video-overlay').fadeIn();
+		$(element).closest('.section').find('.section-overlay').removeClass('hide');
+
+		$(element).text('Play video');
+		$(element).data('play', false);
+	}
+}
+
 function scrollToSection(element) {
 	let target = $(element).data('target');
 
-	scroll.scrollTo(target, 0, 1000, [0.25, 0.0, 0.35, 1.0], false);
+	if (!target) {
+		scroll.scrollTo(0, 0, 1000, [0.25, 0.0, 0.35, 1.0], false);
+	} else {
+		scroll.scrollTo(target, 0, 1000, [0.25, 0.0, 0.35, 1.0], false);
+	}
 }
