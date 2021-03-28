6ad3dd82442670605faad14b1a015ed3acbdf9cb
hunglq249 <luongquochung.249@gmail.com>
2021-03-28 23:28:19 +0700

    update tinymce5 with filemanager

--- public/zyk_admin/js/admin/common.js
+++ public/zyk_admin/js/admin/common.js
@@ -123,28 +123,32 @@ function initFormEvents($form) {
         var editor_config = {
             path_absolute: '/',
             selector: '.tinymce',
-            plugins: ['advlist autolink lists link image charmap print preview anchor textcolor', 'searchreplace visualblocks code fullscreen', 'insertdatetime media table contextmenu paste code help'],
+            plugins: 'advlist autolink lists link image imagetools charmap print preview anchor textcolor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code help',
             toolbar: 'insertfile undo redo | fontselect styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
             height: 500,
             relative_urls: false,
-            file_browser_callback: function (field_name, url, type, win) {
+            file_picker_types: 'file image media',
+            file_picker_callback: function (callback, value, meta) {
                 var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
                 var y = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
 
-                var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-                if (type == 'image') {
+                var cmsURL = editor_config.path_absolute + 'laravel-filemanager?editor=tinymce5&field_name=' + meta.fieldname;
+                if (meta.filetype == 'image') {
                     cmsURL = cmsURL + '&type=Images';
                 } else {
                     cmsURL = cmsURL + '&type=Files';
                 }
 
-                tinyMCE.activeEditor.windowManager.open({
-                    file: cmsURL,
+                tinyMCE.activeEditor.windowManager.openUrl({
+                    url: cmsURL,
                     title: 'Filemanager',
                     width: x * 0.8,
                     height: y * 0.8,
                     resizable: 'yes',
-                    close_previous: 'no'
+                    close_previous: 'no',
+                    onMessage: (api, message) => {
+                        callback(message.content);
+                    }
                 });
             }
         };
